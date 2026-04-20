e681199c9c28f9ddd1e920f9b8c90d1125cd9685
jindong <wlsehdgus23@gmail.com>
2026-04-20 23:30:16 +0900

    init

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ const SITE_URL = "https://jjindong.com";
 const siteUrl = new URL(SITE_URL);
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/", "/jindonghyeon", "/resume", "/trading-floor"];
+  const routes = ["/"];
   const now = new Date();
 
   return routes.map((route) => ({
